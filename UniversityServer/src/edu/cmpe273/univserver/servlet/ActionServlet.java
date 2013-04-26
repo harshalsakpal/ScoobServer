0c651ec020a9ac6ae3b011d72f7abd5641f516a7
harshalsakpal <harshalsakpal@gmail.com>
2013-04-26 12:14:10 -0700

    University Server Commit.

---
+++ UniversityServer/src/edu/cmpe273/univserver/servlet/ActionServlet.java
@@ -0,0 +1,13 @@
+package edu.cmpe273.univserver.servlet;
+
+public class ActionServlet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
