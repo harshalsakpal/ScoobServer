99edd1d3d48f4ca8e77caa16cb0d7275efad3e5c
harshalsakpal <harshalsakpal@gmail.com>
2013-05-06 19:56:38 -0700

    DAO

--- UniversityServer/src/edu/cmpe273/univserver/dao/PersonDAO.java
+++ UniversityServer/src/edu/cmpe273/univserver/dao/PersonDAO.java
@@ -14,13 +14,11 @@ public class PersonDAO {
 		boolean flag = false;
 		DatabaseConnection db = new DatabaseConnection();
 		Connection conn = db.getConnection();
-		String admin = username;
-		String pword = password;
 		try {
 			String sql = "Select username from admin where username=? and password=?";
 			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, admin);
-			ps.setString(2, pword);
+			ps.setString(1, username);
+			ps.setString(2, password);
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
 				flag = true;
@@ -35,7 +33,7 @@ public class PersonDAO {
 			try {
 				conn.close();
 			} catch (SQLException e) {
-
+				
 			}
 		}
 		return flag;
